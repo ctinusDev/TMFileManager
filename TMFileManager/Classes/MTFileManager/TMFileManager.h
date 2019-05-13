c2035ae8dabad0f37f6b0e5be619228b8b7eff73
TomyChen <tomychen@tencent.com>
2019-05-13 14:56:17 +0800

    first commit

---
+++ TMFileManager/Classes/MTFileManager/TMFileManager.h
@@ -0,0 +1,16 @@
+//
+//  TMFileManager.h
+//  Pods-TMFileManager_Example
+//
+//  Created by TomyChen on 2019/5/13.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface TMFileManager : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ TMFileManager/Classes/MTFileManager/TMFileManager.m
@@ -0,0 +1,12 @@
+//
+//  TMFileManager.m
+//  Pods-TMFileManager_Example
+//
+//  Created by TomyChen on 2019/5/13.
+//
+
+#import "TMFileManager.h"
+
+@implementation TMFileManager
+
+@end
